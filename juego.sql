f9399cabc3281e653fb7f160e3b2bf8c87e85c8f
gabrielamayta <gabrielamayta2109@gmail.com>
2024-03-19 16:00:11 -0300

    Avance los primeros pasos del sqlisland

---
+++ juego.sql
@@ -0,0 +1,5 @@
+select * FROM inhabitant
+select * FROM inhabitant WHERE state = "friendly"
+select * FROM inhabitant WHERE job ="weaponsmith" and state = "friendly"
+select * FROM inhabitant WHERE state = "friendly" and job LIKE '%smith'
+SELECT personid from inhabitant where name = "Stranger"
