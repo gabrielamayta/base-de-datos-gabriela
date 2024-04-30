1551c6953dd66b96480ee6b216dd8329be7a1701
gabrielamayta <gabrielamayta2109@gmail.com>
2024-04-30 17:11:21 -0300

    me quede en el 3

---
+++ ejercicioDePrueba.sql
@@ -0,0 +1,8 @@
+Mostrar los nombres de todas las peliculas en alfabeticamente
+SELECT title FROM film
+ORDER by ASC
+
+Mostrar los nombre de todos los ratings de película posibles, en orden alfabético (cuidando de no poner repetidos)
+SELECT DISTINCT title raiting FROM film
+ORDER by ASC
+
